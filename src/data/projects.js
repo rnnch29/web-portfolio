1f613b61a932022ed36f5608e5700ea099381896
rnnch29 <115811785+rnnch29@users.noreply.github.com>
2024-04-29 23:07:44 +0700

    add poke-api project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,11 @@
+import pokemon from '../assets/project/poke-api.png'
+
 export const projects = [
     {
-        // Mockup data 
-        name: 'Title',
-        describe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit placeat eaque veniam nulla repellat?',
-        img: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        link: '#'
+        name: 'Poke-API',
+        describe: 'This project was created to access Pokémon data from an API, allowing users to view details for each Pokémon.',
+        img: pokemon,
+        link: 'https://poke-api-nine-iota.vercel.app/'
     },
 ]
 
