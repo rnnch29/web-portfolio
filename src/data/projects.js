8d0a9f566567d7b2910fe2047a0b3edaea972823
rnnch29 <115811785+rnnch29@users.noreply.github.com>
2024-05-09 13:18:56 +0700

    add rtk-store data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,5 @@
 import pokemon from '../assets/project/poke-api.png'
+import rtkStore from '../assets/project/rtk-store.png'
 
 export const projects = [
     {
@@ -6,6 +7,12 @@ export const projects = [
         describe: 'This project was created to access Pokémon data from an API, allowing users to view details for each Pokémon.',
         img: pokemon,
         link: 'https://poke-api-nine-iota.vercel.app/'
+    },
+    {
+        name: 'RTK-Store',
+        describe: 'RTK-Store project was created to implement the Fake Store API with Redux Toolkit to help with state management.',
+        img: rtkStore,
+        link: 'https://rtk-store-phi.vercel.app/'
     },
 ]
 
