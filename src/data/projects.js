77261e481e095c0e505dccc0661fc8e1ee85c115
rnnch29 <115811785+rnnch29@users.noreply.github.com>
2024-05-15 23:54:44 +0700

    add nextjs project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,6 @@
 import pokemon from '../assets/project/poke-api.png'
 import rtkStore from '../assets/project/rtk-store.png'
+import nextmovie from '../assets/project/nextmovie.png'
 
 export const projects = [
     {
@@ -13,6 +14,12 @@ export const projects = [
         describe: 'RTK-Store project was created to implement the Fake Store API with Redux Toolkit to help with state management.',
         img: rtkStore,
         link: 'https://rtk-store-phi.vercel.app/'
+    },
+    {
+        name: 'Movie-app-next',
+        describe: 'this project was created to implement the omdb API with Nextjs14 + TypeScript.',
+        img: nextmovie,
+        link: 'https://movie-app-next-ts.vercel.app/'
     },
 ]
 
